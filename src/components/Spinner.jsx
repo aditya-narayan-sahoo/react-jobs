1c3e1822788c7a55c58d598fabeb671ad6cc7bf6
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-10-12 01:57:52 +0530

    Add Json-Server and fetch data from the server

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override = {
+  display: "block",
+  margin: "100px auto",
+};
+
+const Spinner = ({ loading }) => {
+  return (
+    <ClipLoader
+      color="#4338ca"
+      loading={loading}
+      cssOverride={override}
+      size={150}
+    />
+  );
+};
+Spinner.propTypes = {
+  loading: PropTypes.bool,
+};
+export default Spinner;
