a09b89ac36dafa1302fcde4b97a9093bfa58750e
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-10-12 01:25:24 +0530

    Modify Job Listings and add Not Found Page

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import {
 import MainLayout from "./layout/MainLayout";
 import HomePage from "./pages/HomePage";
 import JobPage from "./pages/JobPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
 );
