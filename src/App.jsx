c0fd84ca77f0e9a856ff3299abac648287190e8c
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-10-12 10:56:43 +0530

    Add single job page

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,15 @@ import MainLayout from "./layout/MainLayout";
 import HomePage from "./pages/HomePage";
 import JobPage from "./pages/JobPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import SingleJobPage from "./pages/SingleJobPage";
+import { jobLoader } from "./utils/jobLoader";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobPage />} />
+      <Route path="/jobs/:id" element={<SingleJobPage />} loader={jobLoader} />
       <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
